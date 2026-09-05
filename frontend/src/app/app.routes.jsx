d4e96d1dd5ca7dfa56fdbabd9d810356256b5775
vishu <vishwaraj.singh1523@gmail.com>
2026-09-05 18:31:46 +0530

    v2

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import Register from "../features/auth/pages/register";
+import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
 import CreateProduct from "../features/products/pages/CreateProduct";
 import Dashboard from "../features/products/pages/Dashboard";
